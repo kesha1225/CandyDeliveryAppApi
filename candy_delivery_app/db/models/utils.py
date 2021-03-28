083efdd4fc3065a447b765f7c2a6d7ab5c605969
Nickolay Samedov <samedov03@mail.ru>
2021-03-28 13:48:21 +0300

    у меня адвокат акула

--- candy_delivery_app/db/models/utils.py
+++ candy_delivery_app/db/models/utils.py
@@ -17,20 +17,25 @@ def check_courier_can_delivery_by_time(
     #     courier_first_time <= order_first_time < courier_second_time
     #     or courier_first_time < order_second_time <= courier_second_time
     # ):
-    if (
-        (
-            order_first_time < courier_first_time < order_second_time
-            or order_first_time < courier_second_time < order_second_time
-        )
-        or (
-            courier_first_time < order_first_time < courier_second_time
-            or courier_first_time < order_second_time < courier_second_time
-        )
-        or (
-            order_first_time == courier_first_time
-            and order_second_time == courier_second_time
-        )
-    ):
+
+    latest_start = max(order_first_time, courier_first_time)
+    earliest_end = min(order_second_time, courier_second_time)
+    if latest_start < earliest_end:
         return True
+    # if (
+    #     (
+    #         order_first_time < courier_first_time < order_second_time
+    #         or order_first_time < courier_second_time < order_second_time
+    #     )
+    #     or (
+    #         courier_first_time < order_first_time < courier_second_time
+    #         or courier_first_time < order_second_time < courier_second_time
+    #     )
+    #     or (
+    #         order_first_time == courier_first_time
+    #         and order_second_time == courier_second_time
+    #     )
+    # ):
+    #     return True
 
     return False
